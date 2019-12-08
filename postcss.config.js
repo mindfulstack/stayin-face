c942e69750f10d645cd84c120809e523e312fc29
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2019-12-08 21:35:15 +0530

    Add modules to facilitate  efficient HTML authoring

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,15 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    // Specify the paths to all of the template files in your project
+    content: [
+      './src/**/*.html',
+      './src/**/*.svg',
+      './src/**/*.js'
+      // etc.
+    ],
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
 module.exports = {
     plugins: [
     //   require('postcss-font-magician')({
@@ -8,5 +20,8 @@ module.exports = {
       require('tailwindcss'),
       require('postcss-nested'),
       require('autoprefixer'),
+      ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
     ]
 }
--- posthtml.config.js
+++ posthtml.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  "plugins": [
-    require("posthtml-img-autosize")({ root: 'src/assets/img' }),
-    require("posthtml-include")({ root: 'src' }),
-    require("posthtml-expressions")({
-      locals: { foo: 'bar' }
-    }),
-    require('posthtml-extend')({
+  "plugins": {
+    "posthtml-img-autosize": { root: 'src/assets/img' },
+    "posthtml-include": { root: 'src' },
+    "posthtml-extend": {
       encoding: 'utf8', // Parent template encoding (default: 'utf8')
       root: 'src/partials' // Path to parent template directory (default: './')
-    }),
-    require('posthtml-lorem'),
-  ]
+    },
+    "posthtml-expressions":{
+      locals: { statement: '<strong>bar</strong>' }
+    },
+    "posthtml-lorem":{}
+  }
 }
