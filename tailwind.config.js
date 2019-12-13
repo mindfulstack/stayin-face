7836120886c9692ff4c1914727fff7886f99d31e
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2019-12-13 22:01:50 +0530

    Add header, footer, and cards partials

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   theme: {
+    fontFamily: {
+      body: ['Rubik Black', 'sans-serif'],
+      display: ['Lato', 'sans-serif']
+    },
     extend: {}
   },
   variants: {},
-  plugins: []
+  plugins: [
+    function({ addBase, config }) {
+      addBase({
+        'h1': { fontSize: config('theme.fontSize.2xl') },
+        'h2': { fontSize: config('theme.fontSize.xl') },
+        'h3': { fontSize: config('theme.fontSize.lg') },
+      })
+    },
+  ]
 }
